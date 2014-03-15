53007810f1a1e5be7536613847fe91e645355c3d
ja_arczi <ja_arczi@tlen.pl>
2014-03-15 16:17:14 +0000

    Moved BT serial implementation to com.barobot.hardware.serial.jar

---
+++ android/com.barobot.common/src/com/barobot/common/interfaces/AppEventListener.java
@@ -0,0 +1,14 @@
+package com.barobot.common.interfaces;
+
+abstract class AppEventListener {
+	void onCreate() {
+	}
+	void onStart() {
+	}
+	void onPause() {
+	}
+	void onResume() {
+	}
+	void onDestroy() {
+	}	
+}
