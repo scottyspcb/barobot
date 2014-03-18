8c28d6c9dee6b4acbe43fb3f7b9e3b87e75b7793
puermageze@gmail.com <puermageze@gmail.com@cdf298a0-faf9-3d2b-5f3e-60a35f28f79b>
2014-03-18 22:07:06 +0000

    Missing files

---
+++ trunk/android/main_app/src/com/barobot/activity/MenuFragment.java
@@ -0,0 +1,21 @@
+package com.barobot.activity;
+
+
+import com.barobot.R;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MenuFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		 
+		return inflater.inflate(R.layout.fragment_menu, container, false);
+		
+		// TODO: Add onClickListener to all buttons on this fragment
+	}
+}
