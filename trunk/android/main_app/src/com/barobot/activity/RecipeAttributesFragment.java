2b0a478cedeac39852920532b291e8f188837ecf
puermageze@gmail.com <puermageze@gmail.com@cdf298a0-faf9-3d2b-5f3e-60a35f28f79b>
2014-04-08 21:53:33 +0000

    Added Recipe Atrribute Bars and some changes to the IngredientListFragment

---
+++ trunk/android/main_app/src/com/barobot/activity/RecipeAttributesFragment.java
@@ -0,0 +1,20 @@
+package com.barobot.activity;
+
+
+import com.barobot.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecipeAttributesFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		return inflater.inflate(R.layout.fragment_recipe_attributes, container, false);
+	
+		// TODO: Add onClickListener to all buttons on this fragment
+	}
+}
