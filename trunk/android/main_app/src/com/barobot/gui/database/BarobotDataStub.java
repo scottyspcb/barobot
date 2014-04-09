2001ecbd2367b58b0a90e25b9eb050393ab7511d
puermageze@gmail.com <puermageze@gmail.com@cdf298a0-faf9-3d2b-5f3e-60a35f28f79b>
2014-04-09 00:12:22 +0000

    Further changes to GUI

--- trunk/android/main_app/src/com/barobot/gui/database/BarobotDataStub.java
+++ trunk/android/main_app/src/com/barobot/gui/database/BarobotDataStub.java
@@ -37,26 +37,52 @@ public class BarobotDataStub {
 		liq.type = type;
 		liq.insert();
 		
-		liq = new Liquid_t();
-		liq.name = "Smirnoff";
-		liq.type = type;
-		liq.insert();
+		Liquid_t liq2 = new Liquid_t();
+		liq2.name = "Smirnoff";
+		liq2.type = type;
+		liq2.insert();
 		
-		liq = new Liquid_t();
-		liq.name = "Finlandia";
-		liq.type = type;
-		liq.insert();
+		Liquid_t liq3 = new Liquid_t();
+		liq3.name = "Finlandia";
+		liq3.type = type;
+		liq3.insert();
 		
 		Product prod = new Product();
 		prod.capacity = 700;
 		prod.liquid = liq;
 		prod.insert();
 		
+		Product prod2 = new Product();
+		prod2.capacity = 500;
+		prod2.liquid = liq2;
+		prod2.insert();
+		
+		Product prod3 = new Product();
+		prod3.capacity = 100;
+		prod3.liquid = liq3;
+		prod3.insert();
 		
 		Ingredient_t ing = new Ingredient_t();
 		ing.liquid = liq;
 		ing.quantity = 40;
 		ing.insert();
+		
+		Ingredient_t ing2 = new Ingredient_t();
+		ing2.liquid = liq2;
+		ing2.quantity = 60;
+		ing2.insert();
+		
+		Ingredient_t ing3 = new Ingredient_t();
+		ing3.liquid = liq3;
+		ing3.quantity = 20;
+		ing3.insert();
+		
+		Recipe_t rec = new Recipe_t();
+		rec.name = "A long drink name to see how it fits";
+		rec.insert();
+		rec.ingredients.add(ing);
+		rec.ingredients.add(ing2);
+		rec.ingredients.add(ing3);
 		
 		
 		Slot slot = BarobotData.GetSlot(1);
@@ -64,6 +90,18 @@ public class BarobotDataStub {
 		slot.status = "OK";
 		slot.currentVolume = prod.capacity;
 		slot.update();
+		
+		Slot slot3 = BarobotData.GetSlot(3);
+		slot3.product = prod2;
+		slot3.status = "OK";
+		slot3.currentVolume = prod.capacity;
+		slot3.update();
+		
+		Slot slot6 = BarobotData.GetSlot(6);
+		slot6.product = prod3;
+		slot6.status = "OK";
+		slot6.currentVolume = prod.capacity;
+		slot6.update();
 		
 	}
 }
