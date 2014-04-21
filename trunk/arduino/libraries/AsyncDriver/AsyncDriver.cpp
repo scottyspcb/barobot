1badac52ef09c7c19085e5c7e143e3bf4e275527
ja_arczi@tlen.pl <ja_arczi@tlen.pl@cdf298a0-faf9-3d2b-5f3e-60a35f28f79b>
2014-04-21 15:00:51 +0000

    Added imlementation of Async Stepper driver

---
+++ trunk/arduino/libraries/AsyncDriver/AsyncDriver.cpp
@@ -0,0 +1,456 @@
+#include "AsyncDriver.h"
+
+AsyncDriver::AsyncDriver(uint8_t type, uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4){
+	driver_type = type;
+	init();
+	_pin[0] = pin1;
+	_pin[1] = pin2;
+	_pin[2] = pin3;
+	_pin[3] = pin4;
+	pinMode(_pin[0], OUTPUT);
+	pinMode(_pin[1], OUTPUT);
+	pinMode(_pin[2], OUTPUT);
+	pinMode(_pin[3], OUTPUT);
+	enableOutputs();
+}
+AsyncDriver::AsyncDriver(uint8_t pin1, uint8_t pin2, uint8_t newDisablePin ){
+	driver_type = DRIVER;
+	_pin[0]		= pin1;
+	_pin[1] 	= pin2;
+	disablePin	= newDisablePin;
+	init();
+	pinMode(_pin[0], OUTPUT);
+	pinMode(_pin[1], OUTPUT);
+	enableOutputs();
+
+	if (disablePin != 0xff){
+		pinMode(disablePin, OUTPUT);
+		fastWrite(disablePin, true );
+	}
+}
+void AsyncDriver::init(){
+	_currentPos = 0;
+	_targetPos = 0;
+	_speed = 0.0;
+	_maxSpeed = 1.0;
+	_acceleration = 1.0;
+	stepInterval = 0;
+	disablePin = 0xff;
+	lastStepTime = 0;
+	disable_on_ready = false;
+	_n = 0;
+	_c0 = 0.0;
+	_cn = 0.0;
+	_cmin = 1.0;
+	_direction = DIR_ACW;
+	is_disabled = true;
+}
+
+void (*AsyncDriver::user_onReady)(long int);
+
+void AsyncDriver::moveTo(long absolute){
+	//uint8_t oldSREG = SREG;
+	//noInterrupts();
+	if ( _targetPos == absolute ){
+		if( _targetPos ==_currentPos){
+			onReady();
+		}
+	}else{
+		if( disable_on_ready && is_disabled ){
+			enableOutputs();
+		}
+		_targetPos = absolute;
+		computeNewSpeed(true);
+	}
+	//SREG = oldSREG;
+}
+
+// Sets speed to 0
+void AsyncDriver::setCurrentPosition(long position){
+	//uint8_t oldSREG = SREG;
+	//noInterrupts();
+	_targetPos	= position;
+	_currentPos = position;
+	_n			= 0;
+	stepInterval = 0;
+	//SREG = oldSREG;
+}
+
+void AsyncDriver::debug(){
+	volatile long distanceTo = distanceToGo();
+	volatile long stepsToStop = (long)((_speed * _speed) / (2.0 * _acceleration)); // Equation 16
+	Serial.println("-");
+	Serial.println(_n);
+	Serial.println(_speed);
+	Serial.println(_acceleration);
+	Serial.println(_cn);
+	Serial.println(_c0);
+	Serial.println(stepInterval);
+	Serial.println(distanceTo);
+	Serial.println(stepsToStop);
+	Serial.println("-");
+}
+
+void AsyncDriver::fastWrite(uint8_t pin, uint8_t val){
+	uint8_t bit = digitalPinToBitMask(pin);
+	uint8_t port = digitalPinToPort(pin);
+	volatile uint8_t *out;
+	out = portOutputRegister(port);
+	if (val) {
+		*out |= bit;
+	} else {
+		*out &= ~bit;
+	}
+}
+void AsyncDriver::computeNewSpeed( boolean sync ){
+	volatile long distanceTo	= _targetPos - _currentPos;
+	volatile long stepsToStop	= (long)((_speed * _speed) / (2.0 * _acceleration)); // Equation 16
+
+	if (distanceTo == 0 && stepsToStop <= 1){	// We are at the target and its time to stop
+		stepInterval = 0;
+		_speed = 0.0;
+		_n = 0;
+		onReady();
+		return;
+	}
+
+	if (distanceTo > 0){
+		// We are anticlockwise from the target
+		// Need to go clockwise from here, maybe decelerate now
+		if (_n > 0){
+			// Currently accelerating, need to decel now? Or maybe going the wrong way?
+			if ((stepsToStop >= distanceTo) || _direction == DIR_ACW)
+			_n = -stepsToStop; // Start deceleration
+		}else if (_n < 0){
+			// Currently decelerating, need to accel again?
+			if ((stepsToStop < distanceTo) && _direction == DIR_CW)
+			_n = -_n; // Start accceleration
+		}
+	}else if (distanceTo < 0){
+		// We are clockwise from the target
+		// Need to go anticlockwise from here, maybe decelerate
+		if (_n > 0){
+			// Currently accelerating, need to decel now? Or maybe going the wrong way?
+			if ((stepsToStop >= -distanceTo) || _direction == DIR_CW)
+			_n = -stepsToStop; // Start deceleration
+		}else if (_n < 0){
+			// Currently decelerating, need to accel again?
+			if ((stepsToStop < -distanceTo) && _direction == DIR_ACW)
+			_n = -_n; // Start accceleration
+		}
+	}
+	// Need to accelerate or decelerate
+	if (_n == 0){
+		// First step from stopped
+		_cn = _c0;
+		_direction = (distanceTo > 0) ? DIR_CW : DIR_ACW;
+	}else{	// Subsequent step. Works for accel (n is +_ve) and decel (n is -ve).
+		_cn = _cn - ((2.0 * _cn) / ((4.0 * _n) + 1)); // Equation 13
+		_cn = max(_cn, _cmin); 
+	}
+	_n++;
+	stepInterval = _cn;
+	_speed = 1000000.0 / _cn;
+	if (_direction == DIR_ACW){
+		_speed = -_speed;
+	}
+	if(sync){
+	//	debug();
+	//# if 0
+	//# endif
+	}
+}
+
+
+// Implements steps according to the current step interval
+// You must call this at least once per step
+// returns true if a step occurred
+boolean AsyncDriver::haveToRun(){
+	// Dont do anything unless we actually have a step interval
+	if (!stepInterval){
+		return false;
+	}
+
+	volatile unsigned long time = micros();
+	// Gymnastics to detect wrapping of either the nextStepTime and/or the current time
+	volatile unsigned long nextStepTime = lastStepTime + stepInterval;
+
+	if (((nextStepTime >= lastStepTime) && ((time >= nextStepTime) || (time < lastStepTime)))
+	|| ((nextStepTime < lastStepTime) && ((time >= nextStepTime) && (time < lastStepTime)))){
+		if (_direction == DIR_CW){			// Clockwise
+			_currentPos += 1;
+		}else{
+			_currentPos -= 1;	// Anticlockwise
+		}
+		step(_currentPos & 0x7); // Bottom 3 bits (same as mod 8, but works with + and - numbers) 
+		lastStepTime = time;
+		return true;
+	}else{
+		return false;
+	}
+}
+
+void AsyncDriver::setMaxSpeed(float speed){
+	//uint8_t oldSREG = SREG;
+//	noInterrupts();
+	if (_maxSpeed != speed)	{
+		_maxSpeed = speed;
+		_cmin = 1000000.0 / speed;
+		if (_n > 0){					// Recompute _n from current speed and adjust speed if accelerating or cruising
+			_n = (long)((_speed * _speed) / (2.0 * _acceleration)); // Equation 16
+			computeNewSpeed(true);
+		}
+	}
+//	SREG = oldSREG;
+}
+
+// Run the motor to implement speed and acceleration in order to proceed to the target position
+// You must call this at least once per step, preferably in your main loop
+// If the motor is in the desired position, the cost is very small
+// returns true if we are still running to position
+void AsyncDriver::run(){
+	if (haveToRun()){
+		computeNewSpeed(false);
+	}
+}
+
+void AsyncDriver::onReady(){
+	if( disable_on_ready ){
+		disableOutputs();
+	}
+	if(user_onReady){
+		user_onReady( _currentPos );
+	}
+}
+
+void AsyncDriver::setAcceleration(float acceleration){
+	if (acceleration == 0.0){
+		return;
+	}
+	//uint8_t oldSREG = SREG;
+	//noInterrupts();
+	if (_acceleration != acceleration) {
+		// Recompute _n per Equation 17
+		_n = _n * (_acceleration / acceleration);
+		// New c0 per Equation 7
+		_c0 = sqrt(2.0 / acceleration) * 1000000.0;
+		_acceleration = acceleration;
+	}
+	//SREG = oldSREG;
+}
+
+void AsyncDriver::step(uint8_t step){
+	switch (driver_type){
+	case DRIVER:
+		step1(step);
+		break;
+	case FULL4WIRE:
+		step4(step);
+		break;
+	case HALF4WIRE:
+		step8(step);
+		break;
+	}
+}
+
+void AsyncDriver::setOutput4(uint8_t mask){
+	fastWrite(_pin[0], bitRead(mask, 0) );
+	fastWrite(_pin[1], bitRead(mask, 1) );
+	fastWrite(_pin[2], bitRead(mask, 2) );	
+	fastWrite(_pin[3], bitRead(mask, 3) );	
+	last_output = mask;
+}
+
+void AsyncDriver::setOutput2( boolean step, boolean dir ){
+	fastWrite(_pin[0], step );
+	asm("nop");
+	asm("nop");
+	fastWrite(_pin[1], dir );
+	last_output = step;
+}
+
+// 1 pin step function (ie for stepper drivers)
+// This is passed the current step number (0 to 7)
+// Subclasses can override
+void AsyncDriver::step1(uint8_t step){
+	// _pin[0] is step, _pin[1] is direction
+/*
+	uint8_t mask = 0;
+	if( _direction ){
+		mask	= mask + 0b10;
+	}
+	if( step % 2 == 1 ){
+		mask	= mask + 0b01;
+	}
+	setOutput2(mask);*/
+	
+/*
+	if( step % 2 == 1 ){
+		mask = _direction ? 0b11 : 0b01;
+	}else{
+		mask = _direction ? 0b10 : 0b00;
+	}*/
+
+	setOutput2(_direction ? true : false, true );
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	setOutput2(_direction ? true : false, false );
+
+	/*
+	setOutput2(_direction ? 0b11 : 0b01); // step HIGH
+	// Caution 200ns setup time 
+	// Delay the minimum allowed pulse width
+	// delayMicroseconds(_minPulseWidth);
+	// setOutput2(_direction ? 0b10 : 0b00); // step LOW
+	*/
+}
+
+// 4 pin step function for half stepper
+// This is passed the current step number (0 to 7)
+// Subclasses can override
+void AsyncDriver::step4(uint8_t step){
+	volatile uint8_t mask = 0;
+	switch (step & 0x3){
+	case 0:	// 1010
+		mask = 0b0101;
+		break;
+
+	case 1:	// 0110
+		mask = 0b0110;
+		break;
+
+	case 2:	//0101
+		mask = 0b1010;
+		break;
+
+	case 3:	//1001
+		mask = 0b1001;
+		break;
+	}
+	setOutput4(mask);
+}
+
+
+// 4 pin step function
+// This is passed the current step number (0 to 7)
+// Subclasses can override
+void AsyncDriver::step8(uint8_t step){
+	volatile uint8_t mask = 0;
+	switch (step & 0x7){
+	case 0:	// 1000
+		mask = 0b0001;
+		break;
+	case 1:	// 1010
+		mask = 0b0101;
+		break;
+	case 2:	// 0010
+		mask = 0b0100;
+		break;
+	case 3:	// 0110
+		mask = 0b0110;
+		break;
+	case 4:	// 0100
+		mask = 0b0010;
+		break;
+	case 5:	//0101
+		mask = 0b1010;
+		break;
+	case 6:	// 0001
+		mask = 0b1000;
+		break;
+	case 7:	//1001
+		mask = 0b1001;
+		break;
+	}
+	setOutput4(mask);
+}
+
+void	AsyncDriver::disableOutputs(){
+	if(!is_disabled){
+		is_disabled = true;
+		if (driver_type == FULL4WIRE || driver_type == HALF4WIRE){
+			setOutput4(0);
+		}else{
+			setOutput2( false, false );
+		}
+		if (disablePin != 0xff){
+			digitalWrite(disablePin, HIGH );
+		}
+	}
+}
+
+void AsyncDriver::enableOutputs(){
+	if(is_disabled){
+		is_disabled = false;
+		if (disablePin != 0xff){
+			fastWrite(disablePin, LOW );
+		}
+		if (driver_type == FULL4WIRE || driver_type == HALF4WIRE){
+			setOutput4(last_output);
+		}else{
+			setOutput2(last_output, false);
+		}	
+	}
+}
+
+
+void AsyncDriver::stop(){
+	stop( 2.0 );
+}
+
+void AsyncDriver::stop( float multispeed ){
+	if (_speed != 0.0){	
+		volatile long stepsToStop = (long)((_speed * _speed) / (multispeed * _acceleration)) + 1; // Equation 16 (+integer rounding)
+		if (_speed > 0){
+			moveTo(_currentPos + stepsToStop);
+		}else{
+			moveTo(_currentPos - stepsToStop);
+		}
+	}
+}
+
+void AsyncDriver::stopNow(){
+	_targetPos		= _currentPos;
+	stepInterval	= 0;
+	_speed			= 0.0;
+	lastStepTime	= 0;
+	_n				= 0;
+	onReady();
+}
+float AsyncDriver::getMaxSpeed(){
+	return _maxSpeed;
+}
+
+float AsyncDriver::getSpeed(){
+	return _speed;
+}
+
+long AsyncDriver::distanceToGo(){
+	//uint8_t oldSREG = SREG;
+	//noInterrupts();
+	long dist = _targetPos - _currentPos;
+	//SREG = oldSREG;
+	return dist;
+}
+
+long AsyncDriver::targetPosition(){
+	return _targetPos;
+}
+
+long AsyncDriver::currentPosition(){
+	//uint8_t oldSREG = SREG;
+	//noInterrupts();
+	long ppp =  _currentPos;
+	//SREG = oldSREG;
+	return ppp;
+}
+
+// sets event handler function
+void AsyncDriver::setOnReady( void (*fun)(long int) ){
+	user_onReady = fun;
+}
+
+float AsyncDriver::getAcceleration(){
+	return _acceleration;
+}
---
+++ trunk/arduino/libraries/AsyncDriver/AsyncDriver.h
@@ -0,0 +1,79 @@
+#ifndef ASYNC_DRIVER_HEADER
+	#define ASYNC_DRIVER_HEADER
+	#include <stdlib.h>
+	#include <Arduino.h>
+
+	class AsyncDriver{
+	public:
+		typedef enum{
+			DRIVER	= 1,	// Stepper Driver, 2 pins
+			FULL4WIRE = 2,	// 4 wire full stepper, 4 pins
+			HALF4WIRE = 4	// 4 wire half stepper, 4 pins
+		} MotorInterfaceType;
+
+		AsyncDriver(uint8_t type, uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4);
+		AsyncDriver(uint8_t pin1, uint8_t pin2, uint8_t disablePin );
+
+		long	distanceToGo();
+		long	targetPosition();
+		long	currentPosition();
+		float	getSpeed();
+		float	getAcceleration();
+		float	getMaxSpeed();
+		boolean haveToRun();
+		void	moveTo(long absolute); 
+		void	run();
+		void	setMaxSpeed(float speed);
+		void	setAcceleration(float acceleration);
+		void	setSpeed(float speed);
+		void	setCurrentPosition(long position);
+		void	setOnReady( void (*)(long int) );
+		void	stop();
+		void	stop( float multispeed );
+		void	stopNow();
+		void	debug();
+		void	disableOutputs();
+		void	enableOutputs();
+
+		// Disable outputs on ready?
+		volatile boolean		disable_on_ready;
+		volatile boolean		is_disabled;
+
+	protected:
+		typedef enum{
+			DIR_ACW	= 0,// Clockwise
+			DIR_CW	= 1	// Counter-Clockwise
+		} Direction;
+
+		void	computeNewSpeed( boolean sync );
+		void	setOutput2(boolean step, boolean dir );
+		void	setOutput4(uint8_t mask);
+		void	step(uint8_t step);
+		void	step1(uint8_t step);
+		void	step4(uint8_t step);
+		void	step8(uint8_t step);
+		void	fastWrite(uint8_t pin, uint8_t val);
+		void	init();
+		void	onReady();
+
+	private:
+		volatile uint8_t		driver_type;
+		volatile uint8_t		_pin[4];
+		volatile long			_n;
+		volatile long			_currentPos;	// Steps
+		volatile long			_targetPos;		// Steps
+		volatile float			_speed;			// Steps per second
+		volatile float			_acceleration;
+		volatile float			_maxSpeed;
+		volatile float			_c0;
+		volatile float			_cn;
+		volatile float			_cmin;
+		volatile uint8_t		disablePin;
+		volatile uint8_t		last_output;
+		volatile unsigned long	lastStepTime;
+		volatile unsigned long	stepInterval;
+		volatile boolean _direction;
+		static void (*user_onReady)(long int);
+	};
+
+#endif 
