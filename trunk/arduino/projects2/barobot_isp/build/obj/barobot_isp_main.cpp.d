a7babd330120247edb5d001c9b47728a93e9abc1
ja.arczi@gmail.com <ja.arczi@gmail.com@cdf298a0-faf9-3d2b-5f3e-60a35f28f79b>
2014-04-22 21:47:52 +0000

    Repaired paths for arduino libs

--- trunk/arduino/projects2/barobot_isp/build/obj/barobot_isp_main.cpp.d
+++ trunk/arduino/projects2/barobot_isp/build/obj/barobot_isp_main.cpp.d
@@ -2,13 +2,13 @@
  c:\workspace\barobot\arduino\projects2\barobot_isp\build\obj\barobot_isp_main.cpp.o:  \
  c:\workspace\barobot\arduino\projects2\barobot_isp\source\barobot_isp_main.cpp \
   c:\workspace\barobot\arduino\projects2\barobot_isp\source\/barobot_isp_main.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/arduino.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/binary.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/WCharacter.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/WString.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/HardwareSerial.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/Stream.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/Print.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/Printable.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\cores\arduino/new.h \
-  D:\PROG\arduino-1.0.5\hardware\arduino\variants\standard/pins_arduino.h
+  C:\PROG\arduino\hardware\arduino\cores\arduino/arduino.h \
+  C:\PROG\arduino\hardware\arduino\cores\arduino/binary.h \
+  C:\PROG\arduino\hardware\arduino\cores\arduino/WCharacter.h \
+  C:\PROG\arduino\hardware\arduino\cores\arduino/WString.h \
+  C:\PROG\arduino\hardware\arduino\cores\arduino/HardwareSerial.h \
+  C:\PROG\arduino\hardware\arduino\cores\arduino/Stream.h \
+  C:\PROG\arduino\hardware\arduino\cores\arduino/Print.h \
+  C:\PROG\arduino\hardware\arduino\cores\arduino/Printable.h \
+  C:\PROG\arduino\hardware\arduino\cores\arduino/new.h \
+  C:\PROG\arduino\hardware\arduino\variants\standard/pins_arduino.h
