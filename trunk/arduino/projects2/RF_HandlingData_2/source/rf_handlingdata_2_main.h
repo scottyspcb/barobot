2bacdad0b9f4839d4da364d74878b0d3a7652a67
ja.arczi@gmail.com <ja.arczi@gmail.com@cdf298a0-faf9-3d2b-5f3e-60a35f28f79b>
2015-01-05 20:12:30 +0000

    (Tests) Added code for RF radio

---
+++ trunk/arduino/projects2/RF_HandlingData_2/source/rf_handlingdata_2_main.h
@@ -0,0 +1,41 @@
+//-------------------------------------------------------------------
+#ifndef __rf_handlingdata_1_main_H__
+#define __rf_handlingdata_1_main_H__
+//-------------------------------------------------------------------
+ 
+#include <arduino.h>
+#include <RF24_config.h>
+#include <RF24.h>
+#include <printf.h>
+#include <nRF24L01.h>
+#include <printf.h>
+
+//-------------------------------------------------------------------
+ 
+//-------------------------------------------------------------------
+ 
+// Put yout declarations here
+ 
+//-------------------------------------------------------------------
+ 
+//===================================================================
+// -> DO NOT WRITE ANYTHING BETWEEN HERE...
+// 		This section is reserved for automated code generation
+// 		This process tries to detect all user-created
+// 		functions in main_sketch.cpp, and inject their
+// 		declarations into this file.
+// 		If you do not want to use this automated process,
+//		simply delete the lines below, with "&MM_DECLA" text
+//===================================================================
+//---- DO NOT DELETE THIS LINE -- @MM_DECLA_BEG@---------------------
+void loop();
+void setup();
+//---- DO NOT DELETE THIS LINE -- @MM_DECLA_END@---------------------
+// -> ...AND HERE. This space is reserved for automated code generation!
+//===================================================================
+ 
+ 
+//-------------------------------------------------------------------
+#endif
+//-------------------------------------------------------------------
+ 
